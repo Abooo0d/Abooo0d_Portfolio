0013e475feb1adbf69fd1e6b1ef9603e9871dd0b
Abooo0d <abdsadalden2001@gmail.com>
2024-06-21 22:36:22 +0300

    Adding The Tailwindcss ain File ToThe Project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Header, Footer, About, Skills, Testimonial } from "./Container";
+// import "./App.scss";
 const App = () => {
   return (
     <div>
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "cu-primary": "#edf2f8",
+        "cu-secondary": "#313bac",
+        "cu-black": "#030303",
+        "cu-light-gray": "#edf2f8",
+        "cu-gray": "#6b7688",
+        "cu-brown": "#46364a",
+        "cu-white": "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
